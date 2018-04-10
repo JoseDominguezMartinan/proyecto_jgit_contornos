774a9bc83cec0ca6111e98af1b66c2e1102ac93e
JoseDominguezMartinan <jdominguezmartinan@danielcastelao.org>
2018-04-10 19:57:05 +0200

    realizada interfaz grafica y clase main

---
+++ src/main/java/com/mycompany/proyectomavencontornos/ProyectoMavenContornos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectomavencontornos;
+
+/**
+ *
+ * @author jose
+ */
+public class ProyectoMavenContornos {
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+    public static void main(String[] args){
+        Interfaz interfaz=new Interfaz();
+    }
+}
